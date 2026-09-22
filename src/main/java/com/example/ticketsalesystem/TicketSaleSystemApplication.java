474956101d8f8cd4e23f20652ea2c52b4b3e21e7
Jan <jan02tu@gmail.com>
2026-09-22 09:13:40 +0200

    Initialize Commit. Springboot, H2, Flyway

---
+++ src/main/java/com/example/ticketsalesystem/TicketSaleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticketsalesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSaleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketSaleSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticketsalesystem/TicketSaleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketsalesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSaleSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
